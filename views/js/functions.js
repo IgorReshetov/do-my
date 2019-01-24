0022c53e0405a8ff597fa4f1eab8f245974704e5
Yurback <ryv1983@mail.ru>
2019-01-24 21:39:05 +0300

    не работает подключение
    Не работает подключение к локалхост

--- views/js/functions.js
+++ views/js/functions.js
@@ -1,22 +1,20 @@
+function onclick_swap(e){
+    var obj = e.target;
+    var objClass = obj.getAttribute("class");
+    console.log (objClass);
+    // if (e.target.tagName!=='a') e.target.style.display = 'none';
+};
+
+
+
+
+
+
+
+
 // function swap () {
 //     var firstDiv = document.querySelector("p.Ievel5")
 // };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // function name(arg1, arg2) {
