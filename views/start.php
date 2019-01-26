a3720ee891b885bd3c6c402e5fbfd071406cd78e
Yurback <ryv1983@mail.ru>
2019-01-26 03:51:18 +0300

    Завершил верстку 1 страницы

--- views/start.php
+++ views/start.php
@@ -6,6 +6,30 @@ require_once 'views/templates/header.php';
 <div class="wrapper">
     <h1 class = "start-h1">Сервис рекомендаций в выборе жилья</h1>
     <img class = "house" src="views/images/icon/house.svg" alt="multistory building">
+    <h2 class ="start-h2">Эта игра покажет, где вам лучше жить, а где нет</h2>
+    <div class="start-ozenka">Вот какую оценку заслуживает ваш выбор квартиры</div>
+    <div class="start-planeta-img"></div>
+    <img class="start-path" src="views/images/icon/path-start.svg" alt="">
+
+    <div class="start-qustions-flex">
+        <div>
+            <div class="start-qustions-flex-appendix-1"></div>
+            Спорим, вы не до конца знаете, что для вас важно при выборе квартиры?    
+        </div>
+        <div>
+            <div class="start-qustions-flex-appendix-2"></div>
+            Хотите узнать секрет, как работает наш сервис?
+        </div>
+        <div>
+            <div class="start-qustions-flex-appendix-3"></div>
+            Не пора ли завершить начатое и определиться?
+        </div>
+        <div>
+            <div class="start-qustions-flex-appendix-1"></div>
+            Для тех, кто не любит думать своей головой
+        </div>
+    </div>
+
     <!-- <main class = "start-main-block">
         <div class="start-main-block-row-title">  
             <div class="start-main-block-title">Вот, что вы пропустили:</div>
