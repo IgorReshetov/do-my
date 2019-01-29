437740875f5bae5d332fd92eae47519b49b8842f
S.Minakov <sergeyminakov@yandex.ru>
2019-01-29 22:13:08 +0300

    Update Question.php

--- models/Question.php
+++ models/Question.php
@@ -2,38 +2,42 @@
 
 class Question 
 {
-    public $id_tree;
-    public $id_parent; 
-    public $id_child;
+    //public $id_tree;
+    //public $id_parent; 
+    //public $id_child;
     public $id_answer_branch;
     public $question;
     public $questiones;
     public $info;
-    public $image; //нужно добавить в таблицу вопросов картинки, где будут хранится ссылки на картинки
+    // public $image; //нужно добавить в таблицу вопросов картинки, где будут хранится ссылки на картинки
     public $is_must_have_answer;
     public $is_multi_answer;
 
-    public function __construct($id_tree, $id_parent = false, $id_answer_branch = false) //создаем вопрос, если задан только первый аргумент возвращается первый вопрос дерева
+    public function __construct($id_tree, $id_parent = null, $id_answer_branch = null) //создаем вопрос, если задан только первый аргумент возвращается первый вопрос дерева
     {
         global $mysqli; // заводим базу в область видимости
         $id_tree = addslashes($id_tree);
         $id_parent = addslashes($id_parent);
         $id_answer_branch = addslashes($id_answer_branch); //экранируем спецсимволы от sql инъекций
 
-        $query = "call getQuestion($id_tree, $id_parent, $id_answer_branch)"; // функции пишем в верблюжьем стиле
+        # $query = "call getQuestion($id_tree, $id_parent, $id_answer_branch)"; // функции пишем в верблюжьем стиле
+        $query = "call getQuestion(2, null, null)";
         $result = $mysqli->query($query);
 
         if ($result->num_rows > 0) {
             $question_data = $result->fetch_assoc();
-
-            $this->id_parent = $question_data['id_parent'];
-            $this->id_child = $question_data['id_child'];
-            $this->id_answer_branch = $question_data['id_answer_branch'];
-            $this->question = $question_data['question'];
+            
+           $this->id_parent = $question_data['id_parent'];
+           $this->question = $question_data['question'];
+           // $this->id_child = $question_data['id_child'];
+           // $this->id_answer_branch = $question_data['id_answer_branch'];
+           // $this->question = $question_data['question'];
             $this->info = $question_data['info'];
-            $this->image = $question_data['image'];
+            //$this->image = $question_data['image'];
             $this->is_must_have_answer = $question_data['is_must_have_answer'];
-            $this->is_multi_answer = $question_data['is_multi_answer'];
+            $this->is_multi_answer = $question_data['is_multi_aswer'];
+
+            //var_dump($question_data);
         }
     }
 
@@ -42,7 +46,8 @@ class Question
         global $mysqli;
         $id_tree = addslashes($id_tree);
        
-        $query = "call getAllQuestion($id_tree)";
+        $query = "call getAll($id_tree)";
+        // $query = "select * from SPR_QUESTION_TREE where ID_TREE = $id_tree";
         $result = $mysqli->query($query);
 
         if ($result->num_rows > 0) {
