1c54580190396aa2cb5ea9b4c8f604b9afbf3f50
S.Minakov <sergeyminakov@yandex.ru>
2019-01-29 22:12:56 +0300

    Update survey.php

--- controllers/survey.php
+++ controllers/survey.php
@@ -2,13 +2,13 @@
 
 require_once 'models/Question.php';
 
-$question = new Question(1,3);
+$question = new Question(2);
 
-$questiones = Question::getAll(1);
+// $questiones = Question::getAll(2);
 echo "Проверка базы: запрос по 3 вопросу ";
 echo '<pre>';
 var_dump($question);
 
-echo "Проверка базы: запрос всех вопросов";
-echo '<pre>';
-var_dump($questiones);
+// echo "Проверка базы: запрос всех вопросов";
+// echo '<pre>';
+// var_dump($questiones);
