02269a68b1dd750ad4de9c4cf9252bcb80209d47
S.Minakov <sergeyminakov@yandex.ru>
2019-01-31 00:37:48 +0300

    Create Answer.php

---
+++ models/Answer.php
@@ -0,0 +1,38 @@
+<?php
+
+class Answer 
+{
+    public $id_answer;
+    public $answer;
+    public $info;
+    public $is_have_comment;
+    public $is_must_comment;
+    public $is_have_scale;
+    public $scale_min;
+    public $scale_max;
+    public $scale_step;
+    public $show_index;
+
+    public function __construct($id_tree, $id_question)      //варианты ответов для вопроса
+    {
+        global $mysqli;                                      // заводим базу в область видимости
+        $id_tree = addslashes($id_tree);
+        $id_question = addslashes($id_question);             //экранируем спецсимволы от sql инъекций
+     
+        $query = "call getAnswer($id_tree, $id_question)";
+        $result = $mysqli->query($query);
+
+        while ($answer_data = $result->fetch_assoc()) {
+             $this->id_answer[] =           $answer_data['id_answer'];
+             $this->answer[] =              $answer_data['answer'];
+             $this->info[] =                $answer_data['info'];
+             $this->is_have_comment[] =     $answer_data['is_have_comment'];
+             $this->is_must_comment[] =     $answer_data['is_must_comment'];
+             $this->is_have_scale[] =       $answer_data['is_have_scale'];
+             $this->scale_min[] =           $answer_data['scale_min'];
+             $this->scale_max[] =           $answer_data['scale_max'];
+             $this->scale_step[] =          $answer_data['scale_step'];
+             $this->show_index[] =          $answer_data['show_index'];
+        }
+    }
+}
