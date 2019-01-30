0aac609d5b5b07c85a87170a7589a5ca8497b5e5
S.Minakov <sergeyminakov@yandex.ru>
2019-01-31 00:37:59 +0300

    Update Question.php

--- models/Question.php
+++ models/Question.php
@@ -2,42 +2,31 @@
 
 class Question 
 {
-    //public $id_tree;
-    //public $id_parent; 
-    //public $id_child;
     public $id_answer_branch;
     public $question;
     public $questiones;
     public $info;
-    // public $image; //нужно добавить в таблицу вопросов картинки, где будут хранится ссылки на картинки
     public $is_must_have_answer;
     public $is_multi_answer;
 
-    public function __construct($id_tree, $id_parent = null, $id_answer_branch = null) //создаем вопрос, если задан только первый аргумент возвращается первый вопрос дерева
+    public function __construct($id_tree, $id_parent = 0, $id_answer_branch = 0) //создаем вопрос, если задан только первый аргумент возвращается первый вопрос дерева
     {
-        global $mysqli; // заводим базу в область видимости
+        global $mysqli;                                                          // заводим базу в область видимости
         $id_tree = addslashes($id_tree);
         $id_parent = addslashes($id_parent);
-        $id_answer_branch = addslashes($id_answer_branch); //экранируем спецсимволы от sql инъекций
+        $id_answer_branch = addslashes($id_answer_branch);                       //экранируем спецсимволы от sql инъекций
 
-        # $query = "call getQuestion($id_tree, $id_parent, $id_answer_branch)"; // функции пишем в верблюжьем стиле
-        $query = "call getQuestion(2, null, null)";
+        $query = "call getQuestion($id_tree, $id_parent, $id_answer_branch)";
         $result = $mysqli->query($query);
 
         if ($result->num_rows > 0) {
             $question_data = $result->fetch_assoc();
             
-           $this->id_parent = $question_data['id_parent'];
-           $this->question = $question_data['question'];
-           // $this->id_child = $question_data['id_child'];
-           // $this->id_answer_branch = $question_data['id_answer_branch'];
-           // $this->question = $question_data['question'];
-            $this->info = $question_data['info'];
-            //$this->image = $question_data['image'];
+            $this->id_parent =           $question_data['id_parent'];
+            $this->question =            $question_data['question'];
+            $this->info =                $question_data['info'];
             $this->is_must_have_answer = $question_data['is_must_have_answer'];
-            $this->is_multi_answer = $question_data['is_multi_aswer'];
-
-            //var_dump($question_data);
+            $this->is_multi_answer =     $question_data['is_multi_aswer'];
         }
     }
 
