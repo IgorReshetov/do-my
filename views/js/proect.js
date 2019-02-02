af0c26ce11d70a51cf7ae5ccfe552dac10cebf01
Igor Reshetov <ivreshetov@mail.ru>
2019-02-02 03:58:11 +0300

    Done без mobile

--- views/js/proect.js
+++ views/js/proect.js
@@ -1,5 +1,56 @@
 window.onload = init;
 function init() {
+    // обработчики нажатия на вопрос о проекте
+    var left_qw = document.getElementById('first').children[1].getAttribute('left');
+    var right_qw = document.getElementById('first').children[1].getAttribute('right');
+
+    if (left_qw == 1){
+        var button1 = document.getElementById('first').children[1].children[0].children[0];
+        button1.onclick = func1;
+        
+        
+        function func1() {
+            this.classList.add('display-none');
+            this.nextElementSibling.classList.add('display-block');
+            document.getElementById('first').children[1].setAttribute('left', 2);
+            init();
+            return; 
+        }
+    }
+
+    if (right_qw == 1){
+        var button2 = document.getElementById('first').children[1].children[2].children[0];
+        button2.onclick = func2;
+        
+
+        function func2() {
+            this.classList.add('display-none');
+            this.nextElementSibling.classList.add('display-block');
+            document.getElementById('first').children[1].setAttribute('right', 2);
+            init();
+            return; 
+        }
+    }
+
+    // проверяем отвечены ли вопросы о проекте
+
+    if ((left_qw == 2 && right_qw != 2) || (right_qw == 2 && left_qw != 2 )){
+        document.getElementById('first').children[1].children[1].children[0].classList.add('opacity07');
+    }
+
+    
+    if (left_qw == 2 && right_qw == 2){
+        document.getElementById('first').children[1].children[1].children[0].classList.add('display-none');
+        if (document.getElementById('first').children[2].children[0].children[0].classList.contains('display-none') == true){
+        document.getElementById('first').children[2].children[0].children[0].classList.remove('display-none');
+        }
+        opros();
+        
+    }
+}
+
+// Функция опроса
+function opros() {
     // определяем на каком вопросе о команде остановился пользователь
     var start_elem = document.getElementById('first');
     start_elem = start_elem.children[2];
@@ -12,46 +63,78 @@ function init() {
         start_elem = start_elem.nextElementSibling;
     }
     
-    //обработчик нажатия на вопрос
-    var button = document.getElementById('first').children[point+1].children[0];
+//обработчик нажатия на вопрос о команде
     
-    button.addEventListener('click', func);
+    if (point != undefined){
+    var button3 = document.getElementById('first').children[point+1].children[0];
+    button3.onclick = func3;
     
-    function func() {
+    function func3() {
         question(point);
-        button.removeEventListener('click', func);
-        init(); 
-    };
-     
+        opros();
+        return; 
+    }
+    }
 }
+
 
 // функция отработки нажатия на вопрос
 function question(point) {
     var start_elem = document.getElementById('first');
     start_elem = start_elem.children[1+point];
     var check = Number.parseInt(start_elem.getAttribute('left'));
+    if (check > 0) {
+        var slogan = Number.parseInt(start_elem.children[0].children[check-1].getAttribute('row'));
+        if (slogan == 0){return false}
+    }
+    if (check == 0) {return false};
     var row = Number.parseInt(start_elem.children[0].children[check-1].getAttribute('row'));
     var pos = Number.parseInt(start_elem.children[0].children[check-1].getAttribute('pos'));
-    if (point == 1 && row != 5) {check=check+1};
-    console.log (check);
     start_elem.children[0].children[check-1].classList.add('display-none');
     start_elem.setAttribute('left', 0);
     if (row == 0) {return false};
     var start_elem = document.getElementById('first');
     start_elem = start_elem.children[row+1];
+    
+    start_elem.previousElementSibling.scrollIntoView(true);
+    
     if (pos > 1) {start_elem.children[2].children[pos-2].classList.add('display-none')};
     start_elem.children[2].children[pos-1].classList.add('display-block');
-    start_elem.setAttribute('left', pos);
     if (row == 1) {pos=pos+1};
     start_elem.children[0].children[pos-1].classList.add('display-block');
     start_elem.setAttribute('left', pos);
+    start_elem.children[1].children[0].classList.add('opacity07');
+    if (row == 1 && pos == 4) {show()}
     return;
 }
 
-
-
-
-
+// функция финального показ
+function show(){
+    var start_elem = document.getElementById('first');
+    window.setTimeout(function(){
+    start_elem.children[2].children[2].children[2].classList.add('display-none');
+    start_elem.children[2].children[1].children[0].classList.add('display-none');
+    start_elem.children[2].children[2].children[3].classList.add('display-block');
+    }, 2000);
+    window.setTimeout(function(){
+    start_elem.children[3].previousElementSibling.scrollIntoView(true);
+    start_elem.children[3].children[0].children[3].classList.add('display-block');
+    start_elem.children[3].children[1].children[0].classList.add('display-none');
+    start_elem.children[3].children[2].children[2].classList.add('display-none');
+    start_elem.children[3].children[2].children[3].classList.add('display-block');
+    }, 4000);
+    for (let index = 1; index < 5; index++) {
+        window.setTimeout(function(){
+        var row = index+3;
+        start_elem.children[row].previousElementSibling.scrollIntoView(true);
+        start_elem.children[row].children[0].children[2].classList.add('display-block');
+        start_elem.children[row].children[1].children[0].classList.add('display-none');
+        start_elem.children[row].children[2].children[1].classList.add('display-none');
+        start_elem.children[row].children[2].children[2].classList.add('display-block');
+        }, 4000+index*2000);
+    }
+    return;
+}
 
 
 
--- views/proect.php
+++ views/proect.php
@@ -6,7 +6,7 @@ require_once 'views/templates/header.php';
     <h1 class="proect-h1">Рекомендательный сервис выбора жилья</h1>
     <div class="row-proect" left="<?=$proect_left[0]?>" right="<?=$proect_right[0]?>">
         <div class="column1-proect column-proect">
-            <div class="column-proect-text <?=($proect_left[0] == 1)?'display-block':''?>">Зачем нужен ваш сервис?</div> 
+            <div class="column-proect-text <?=($proect_left[0] == 1)?'display-block':''?>">Зачем нужен сервис?</div> 
             <div class="column-proect-text <?=($proect_left[0] == 2)?'display-block':''?>">Мы точно скажем вам, где лучше жить.</div>
         </div>
         <div class="column2-proect column-proect column2-image1"><img src="views/images/icon/question_ring.svg" class="<?=($proect_left[0]+$proect_right[0] > 2)?'opacity07':''?> <?=($proect_left[0]+$proect_right[0] > 3)?'display-none':''?>"></div>
@@ -18,7 +18,7 @@ require_once 'views/templates/header.php';
 
     <div class="row-proect" left="<?=$proect_left[1]?>" right="<?=$proect_right[1]?>">
         <div class="column1-proect column-proect">
-            <div class="column-proect-text <?=($proect_left[1] == 1)?'display-block':''?>" row ="5" pos = "1">Кто в проекте больше всех заботится о пользователях?</div> 
+            <div class="column-proect-text <?=($proect_left[1] == 1)?'display-block':''?> display-none" row ="5" pos = "1">Кто в проекте больше всех заботится о пользователях?</div> 
             <div class="column-proect-text <?=($proect_left[1] == 2)?'display-block':''?>" row ="2" pos = "1">У нас есть один ученный, где он?</div>
             <div class="column-proect-text <?=($proect_left[1] == 3)?'display-block':''?>" row ="2" pos = "3">Кто у нас главный по тарелочкам?</div>
             <div class="column-proect-text <?=($proect_left[1] == 4)?'display-block':''?>" row ="0" pos = "0"><i>"Чтобы видеть дальше других, нужно стоять на плечах гигантов"</i></div>
@@ -36,8 +36,8 @@ require_once 'views/templates/header.php';
         <div class="column1-proect column-proect">
             <div class="column-proect-text <?=($proect_left[2] == 1)?'display-block':''?>" row ="4" pos ="1">Кто по ночам пишет на нативном Java Script?</div> 
             <div class="column-proect-text <?=($proect_left[2] == 2)?'display-block':''?>" row ="4" pos = "2">Разве юрист и compliance может быть фронтовиком?</div>
-            <div class="column-proect-text <?=($proect_left[2] == 3)?'display-block':''?>" row ="1" pos = "2">Кто в команде может не выполнить задачу спринта, но при этом выиграть войну?</div>
-            <div class="column-proect-text <?=($proect_left[2] == 4)?'display-block':''?>" row ="0" pos = "0"><i>"Чтобы видеть дальше других, нужно стоять на плечах гигантов"</i></div>
+            <div class="column-proect-text <?=($proect_left[2] == 3)?'display-block':''?>" row ="1" pos = "3">Кто в команде может не выполнить задачу спринта, но при этом выиграть войну?</div>
+            <div class="column-proect-text <?=($proect_left[2] == 4)?'display-block':''?>" row ="0" pos = "0"><i>"Ничто так не способствует успешному внедрению новшеств, как отсутствие проверок."</i></div>
         </div>
         <div class="column2-proect column-proect column2-image3"><img src="views/images/icon/question_ring.svg" class="<?=($proect_right[2] > 0)?'opacity07':''?> <?=($proect_left[2]+$proect_right[2] == 7)?'display-none':''?>"></div>
         <div class="column3-proect column-proect">
@@ -52,9 +52,9 @@ require_once 'views/templates/header.php';
         <div class="column1-proect column-proect">
             <div class="column-proect-text <?=($proect_left[3] == 1)?'display-block':''?>" row ="6" pos ="1">Этот человек не имеет права на ошибку, он наш последний рубеж?</div> 
             <div class="column-proect-text <?=($proect_left[3] == 2)?'display-block':''?>" row ="1" pos = "2">У нас есть сапер в команде - он может ошибится только один раз?</div>
-            <div class="column-proect-text <?=($proect_left[3] == 3)?'display-block':''?>" row ="0" pos = "0"><i>"Чтобы видеть дальше других, нужно стоять на плечах гигантов"</i></div>
+            <div class="column-proect-text <?=($proect_left[3] == 3)?'display-block':''?>" row ="0" pos = "0"><i>"Ждем слоган от Стаса"</i></div>
         </div>
-        <div class="column2-proect column-proect column2-image3"><img src="views/images/icon/question_ring.svg" class="<?=($proect_right[3] > 0)?'opacity07':''?> <?=($proect_left[3]+$proect_right[3] == 5)?'display-none':''?>"></div>
+        <div class="column2-proect column-proect column2-image4"><img src="views/images/icon/question_ring.svg" class="<?=($proect_right[3] > 0)?'opacity07':''?> <?=($proect_left[3]+$proect_right[3] == 5)?'display-none':''?>"></div>
         <div class="column3-proect column-proect">
             <div class="column-proect-text <?=($proect_right[3] == 1)?'display-block':''?>"><b>Станислав:</b> SEO оптимизация и продвижение проекта.</div>
             <div class="column-proect-text <?=($proect_right[3] == 2 && $proect_left[3]+$proect_right[3] < 6)?'display-block':''?>"><b>Станислав:</b> директор по марктетингу.</div>
@@ -66,9 +66,9 @@ require_once 'views/templates/header.php';
         <div class="column1-proect column-proect">
             <div class="column-proect-text <?=($proect_left[4] == 1)?'display-block':''?>" row ="3" pos = "1">Он продвигает нас вперед и следит за пауками?</div> 
             <div class="column-proect-text <?=($proect_left[4] == 2)?'display-block':''?>" row ="6" pos = "2">У него нет незаменимых, но они решают все?</div>
-            <div class="column-proect-text <?=($proect_left[4] == 3)?'display-block':''?>" row ="0" pos = "0"><i>"Чтобы видеть дальше других, нужно стоять на плечах гигантов"</i></div>
+            <div class="column-proect-text <?=($proect_left[4] == 3)?'display-block':''?>" row ="0" pos = "0"><i>"Мы никогда не узнаем то что ищем, пока не найдем это"</i></div>
         </div>
-        <div class="column2-proect column-proect column2-image4"><img src="views/images/icon/question_ring.svg" class="<?=($proect_right[4] > 0)?'opacity07':''?> <?=($proect_left[4]+$proect_right[4] == 5)?'display-none':''?>"></div>
+        <div class="column2-proect column-proect column2-image5"><img src="views/images/icon/question_ring.svg" class="<?=($proect_right[4] > 0)?'opacity07':''?> <?=($proect_left[4]+$proect_right[4] == 5)?'display-none':''?>"></div>
         <div class="column3-proect column-proect">
             <div class="column-proect-text <?=($proect_right[4] == 1)?'display-block':''?>"><b>Юрий:</b> разработчик front-end.</div>
             <div class="column-proect-text <?=($proect_right[4] == 2 && $proect_left[4]+$proect_right[4] < 5)?'display-block':''?>"><b>Юрий:</b> compliance, правовая защита, безопасноть.</div>
@@ -80,9 +80,9 @@ require_once 'views/templates/header.php';
         <div class="column1-proect column-proect">
             <div class="column-proect-text <?=($proect_left[5] == 1)?'display-block':''?>" row = "1" pos = "1">Кто среди нас больше всех любит объекты и кует победу в тылу?</div> 
             <div class="column-proect-text <?=($proect_left[5] == 2)?'display-block':''?>" row ="2" pos ="2">В его руках все богатство команды?</div>
-            <div class="column-proect-text <?=($proect_left[5] == 3)?'display-block':''?>" row ="0" pos = "0"><i>"Чтобы видеть дальше других, нужно стоять на плечах гигантов"</i></div>
+            <div class="column-proect-text <?=($proect_left[5] == 3)?'display-block':''?>" row ="0" pos = "0"><i>"Успех - это умение двигаться от одной неудачи к другой, не теряя энтузиазма"</i></div>
         </div>
-        <div class="column2-proect column-proect column2-image5"><img src="views/images/icon/question_ring.svg" class="<?=($proect_right[5] > 0)?'opacity07':''?> <?=($proect_left[5]+$proect_right[5] == 5)?'display-none':''?>"></div>
+        <div class="column2-proect column-proect column2-image6"><img src="views/images/icon/question_ring.svg" class="<?=($proect_right[5] > 0)?'opacity07':''?> <?=($proect_left[5]+$proect_right[5] == 5)?'display-none':''?>"></div>
         <div class="column3-proect column-proect">
             <div class="column-proect-text <?=($proect_right[5] == 1)?'display-block':''?>"><b>Татьяна:</b> UI/UX дизайнер.</div>
             <div class="column-proect-text <?=($proect_right[5] == 2 && $proect_left[5]+$proect_right[5] < 5)?'display-block':''?>"><b>Татьяна:</b> администратор команды.</div>
@@ -94,9 +94,9 @@ require_once 'views/templates/header.php';
         <div class="column1-proect column-proect">
             <div class="column-proect-text <?=($proect_left[6] == 1)?'display-block':''?>" row ="5" pos = "2">Кто в нашей команде самый пунктуальный?</div> 
             <div class="column-proect-text <?=($proect_left[6] == 2)?'display-block':''?>" row ="3" pos = "2">Он знает, что нужно продавать не сверла, а отверстия - определенного диаметра?</div>
-            <div class="column-proect-text <?=($proect_left[6] == 3)?'display-block':''?>" row ="0" pos = "0"><i>"Чтобы видеть дальше других, нужно стоять на плечах гигантов"</i></div>
+            <div class="column-proect-text <?=($proect_left[6] == 3)?'display-block':''?>" row ="0" pos = "0"><i>"Только те, кто предпринимают абсурдные попытки, смогут достичь невозможного"</i></div>
         </div>
-        <div class="column2-proect column-proect column2-image6"><img src="views/images/icon/question_ring.svg" class="<?=($proect_right[6] > 0)?'opacity07':''?> <?=($proect_left[6]+$proect_right[6] == 5)?'display-none':''?>"></div>
+        <div class="column2-proect column-proect column2-image7"><img src="views/images/icon/question_ring.svg" class="<?=($proect_right[6] > 0)?'opacity07':''?> <?=($proect_left[6]+$proect_right[6] == 5)?'display-none':''?>"></div>
         <div class="column3-proect column-proect">
             <div class="column-proect-text <?=($proect_right[6] == 1)?'display-block':''?>"><b>Владимир:</b> тестирование ПО.</div>
             <div class="column-proect-text <?=($proect_right[6] == 2 && $proect_left[6]+$proect_right[6] < 5)?'display-block':''?>"><b>Владимир:</b> HR директор.</div>
