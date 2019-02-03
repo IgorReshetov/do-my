760c232fad37131e42607f390c7e2285cac357e9
Igor Reshetov <ivreshetov@mail.ru>
2019-02-03 21:35:26 +0300

    Помощь

--- views/start.php
+++ views/start.php
@@ -5,21 +5,23 @@ require_once 'views/templates/header.php';
 
     <div class="wrapper">
         <h1 class = "start-h1">Сервис рекомендаций в выборе жилья</h1> <!--ИСПP-->
-        <img class = "house" src="views/images/icon/house.svg" alt="multistory building"> <!--ИСПP-->
+         <!--ИСПP-->
         <h2 class ="start-h2">Вот какую оценку заслуживает ваш выбор квартиры
             <div class="start-h2-ozenka">7.8</div>
         </h2>
         <div class="start-ozenka">Эта игра покажет, где вам лучше жить, а где нет</div>
-        <div class="start-planeta-img"></div>
         <div class="start-qustions-1">
             <div class="start-qustions-flex-appendix-1"></div>
             Спорим, вы не до конца знаете, что для вас важно при выборе квартиры?    
         </div>
          <div class="start-linia">
-                <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 400 500" preserveAspectRatio="none">
-                <path width="100%"  height="300px" fill="none" stroke="#FF3535"  stroke-width="5" d="M40,300 Q100,500 400,400" />
+                <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 400 700" preserveAspectRatio="none">
+                <path width="100%"  height="500px" fill="none" stroke="#FF3535"  stroke-width="5" d="M20,300 Q100,700, 380,670" />
                 </svg>
+            <div class="start-planeta-img"></div>
+            <img class = "house" src="views/images/icon/house.svg" alt="multistory building">
         </div>
+        
     </div>
 
 
