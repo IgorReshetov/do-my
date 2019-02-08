4146ba6781cb6970a15ec5254b2edb1fea799321
Yurback <ryv1983@mail.ru>
2019-02-09 02:24:11 +0300

    Финальная версия 1-ой страницы

--- views/start.php
+++ views/start.php
@@ -5,18 +5,21 @@ require_once 'views/templates/header.php';
 
     <div class="wrapper">
         <h1 class = "start-h1">Сервис рекомендаций в выборе жилья</h1> 
-      
-        <div class="start-h2-conteiner">
-            <h2 class ="start-h2">Вот какую оценку заслуживает ваш выбор квартиры
+        <div class="start-linia-2">
+            <div class="start-h2-conteiner">
+                <h2 class ="start-h2">Вот какую оценку заслуживает ваш выбор квартиры
                 <div class="start-h2-ozenka">7.8</div>
             </h2>
             <h2 class="start-ozenka">Эта игра покажет, где вам лучше жить, а где нет</h2>
-        </div>
+            </div>
 
-        <div class="start-qustions-1">
-            <div class="start-qustions-flex-appendix-1"></div>
-            Спорим, вы не до конца знаете, что для вас важно при выборе квартиры?    
+            <div class="start-qustions-1">
+                <div class="start-qustions-flex-appendix-1"></div>
+                Спорим, вы не до конца знаете, что для вас важно при выборе квартиры?    
+            </div>
+        
         </div>
+       
 
          <div class="start-linia">
             <svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 400 700" preserveAspectRatio="none">
