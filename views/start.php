050875f67a6d346be31aa17e94247ba9b6d8a3f0
Igor Reshetov <ivreshetov@mail.ru>
2019-02-10 00:04:19 +0300

    Внес редакцию, что успел.
    Осталовь еще много мелких несоответствий.

--- views/start.php
+++ views/start.php
@@ -9,13 +9,13 @@ require_once 'views/templates/header.php';
             <div class="start-h2-conteiner">
                 <h2 class ="start-h2">Вот какую оценку заслуживает ваш выбор квартиры
                 <div class="start-h2-ozenka">7.8</div>
-            </h2>
-            <h2 class="start-ozenka">Эта игра покажет, где вам лучше жить, а где нет</h2>
+                </h2>
+                <h2 class="start-ozenka">Эта игра покажет, где вам лучше жить, а где нет</h2>
             </div>
 
             <div class="start-o-proecte">
                 <div class="start-qustions-flex-appendix-1"></div>
-                <a href="index.php?page=project">Информация о проекте</a>
+                <a href="index.php?page=project" class= 'start-qustions-2'>Хотите узнать секрет как работает наш сервис?</a>
             </div> 
 
             <div class="start-qustions-1">
@@ -35,63 +35,6 @@ require_once 'views/templates/header.php';
         </div>
         
     </div>
-
-
-
-    <!-- __________________________________________________________________________________ -->
-    <!-- <div class="start-qustions-flex">
-        <div>
-            <div class="start-qustions-flex-appendix-1"></div>
-            Спорим, вы не до конца знаете, что для вас важно при выборе квартиры?    
-        </div>
-        <div>
-            <div class="start-qustions-flex-appendix-2"></div>
-            Хотите узнать секрет, как работает наш сервис?
-        </div>
-        <div>
-            <div class="start-qustions-flex-appendix-3"></div>
-            Не пора ли завершить начатое и определиться?
-        </div>
-        <div>
-            <div class="start-qustions-flex-appendix-1"></div>
-            Для тех, кто не любит думать своей головой
-        </div>
-    </div> -->
-
-
-
-    <!-- ____________________________________________________________________________ -->
-    <!-- <main class = "start-main-block">
-        <div class="start-main-block-row-title">  
-            <div class="start-main-block-title">Вот, что вы пропустили:</div>
-            <div class="start-main-block-icon"></div>
-        </div>
-        <div class="start-main-block-row">
-            <div class="start-main-block-icon"></div>
-            <h2 class ="start-main-block-h2"> Какую оценку заслуживает ваш выбор квартиры?</h2>
-        </div>
-        <div class="start-main-block-row">
-            <div class="start-main-block-icon"></div>
-            <h2 class ="start-main-block-h2"> Эта игра покажет, где вам лучше жить, а где нет.</h2>
-        </div>
-        <div class="start-main-block-row">
-            <div class="start-main-block-icon"></div>
-            <h2 class ="start-main-block-h2"> Спорим, вы не до конца знаете, что для вас важно при выборе квартиры?</h2>
-        </div>
-        <div class="start-main-block-row">
-            <div class="start-main-block-icon"></div>
-            <h2 class ="start-main-block-h2"> Какую оценку заслуживает ваш выбор квартиры?</h2>
-        </div>
-        <div class="start-main-block-row">
-            <div class="start-main-block-icon"></div>
-            <h2 class ="start-main-block-h2"> Хотите узнать секрет, как работает наш сервис?</h2>
-        </div>
-        <div class="start-main-block-row">
-            <div class="start-main-block-icon"></div>
-            <h2 class ="start-main-block-h2"> Для тех, кто не любит думать своей головой.</h2>
-        </div> -->
-
-    <!-- </main> -->
     
 
 <?php
