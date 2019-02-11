2bfa3086d89aaeeb2f113a604164c0c04f19e19c
Yurback <ryv1983@mail.ru>
2019-02-12 01:51:48 +0300

    Написал  запрос JSON  в файле  function.js

--- views/js/functions.js
+++ views/js/functions.js
@@ -8,8 +8,34 @@ function onclick_flowQuestion(e){
         setTimeout(function () {document.querySelector(".start-qustions-1").style.display="block"},400);
     }
     console.log (objClass);
-   
 };
+
+// Создаем обработчик для отправки запроса JSON
+
+function json_Q_A() {
+    var url = "/controllers/handlers/sample.php";
+    var request = new XMLHttpRequest();
+    request.open("POST", url);
+    request.onload = function() { 
+        if (request.status == 200) { 
+            update_Q_A(request.responseText);
+        }
+    };
+    var number_Q = JSON.stringify(1)    // (Выбираем ID элемента DOM, на который щелкнул user и парсим parseInt(str.match(/\d+/) ) // 
+    request.send(number_Q)
+}
+
+function update_Q_A (responseText) {
+    var div_Q_A = document.getElementById("start-Q-A") // Выбираем Блок для вставки ответов для юзера
+    var obj_Q_A = JSON.parse(responseText); // Должен придти массив с ответами []
+    var div = document.createElement("div");
+    div.setAttribute("class", "start-Answer");
+    div.innerHTML= obj_Q_A[0];
+    div_Q_A.appendChild(div);
+}
+
+
+
 
 
 
