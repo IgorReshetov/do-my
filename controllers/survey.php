a61b41f65868d945fe5aaacfe55dda50507fc26a
Igor Reshetov <ivreshetov@mail.ru>
2019-02-12 23:25:49 +0400

    Доделал обработчик выдачи вопроса с ответами на фронт

--- controllers/survey.php
+++ controllers/survey.php
@@ -3,23 +3,10 @@ require_once 'models/Question.php';
 require_once 'models/Answer.php';
 
 
-$question = new Question(1);
+$question = new Question(1, 3);
 
 
 $answer = new Answer(1, $question->id_parent);
 
-echo '<br>';
-echo '<br>';
-echo '<br>';
-echo '<br>';
-echo '<br>';
-echo '<br>';
-echo '<br>';
-echo '<pre>';
-var_dump ($question);
-
-echo '<br>';
-echo '<pre>';
-var_dump ($answer);
 
 require_once 'views/survey.php';
