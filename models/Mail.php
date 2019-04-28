9fa631ee90d178f53446ca5e26a5be2594e8caf2
IgorReshetov <ivreshetov@mail.ru>
2019-04-29 00:18:28 +0300

    Завершил полностью проход пользователя до финальной страницы

--- models/Mail.php
+++ models/Mail.php
@@ -37,40 +37,11 @@ class Mail
             $bodyMail = $message;
         }
 
-        $this->result= mail($mailTo, $subject, $bodyMail, $headers); // отправка письма
         
+        $this->result= mail($mailTo, $subject, $bodyMail, $headers); // отправка письма
+      
 
     }
 
-
-
-
-
-        // $separator = "---"; // разделитель в письме
-        // // Заголовки для письма
-        // $headers = "MIME-Version: 1.0\r\n";
-        // $headers .= "From: $from\nReply-To: $from\n"; // задаем от кого письмо
-        // $headers .= "Content-Type: multipart/mixed; boundary=\"$separator\""; // в заголовке указываем разделитель
-        // // если письмо с вложением
-        // if($file){
-        //     $bodyMail = "--$separator\n"; // начало тела письма, выводим разделитель
-        //     $bodyMail .= "Content-Type:text/html; charset=\"utf-8\"\n"; // кодировка письма
-        //     $bodyMail .= "Content-Transfer-Encoding: 7bit\n\n"; // задаем конвертацию письма
-        //     $bodyMail .= $message."\n"; // добавляем текст письма
-        //     $bodyMail .= "--$separator\n";
-        //    $fileRead = fopen($file, "r"); // открываем файл
-        //     $contentFile = fread($fileRead, filesize($file)); // считываем его до конца
-        //     fclose($fileRead); // закрываем файл
-        //     $bodyMail .= "Content-Type: {application/pdf}; \n name==?utf-8?B?".base64_encode(basename($file))."?=\n"; 
-        //     $bodyMail .= "Content-Transfer-Encoding: base64\n"; // кодировка файла
-        //     $bodyMail .= "Content-Disposition: attachment; filename==?utf-8?B?".base64_encode(basename($file))."?=\n\n";
-        //     $bodyMail .= chunk_split(base64_encode($contentFile))."\n"; // кодируем и прикрепляем файл
-        //     $bodyMail .= "--".$separator ."--\n";
-        // // письмо без вложения
-        // }else{
-        //     $bodyMail = $message;
-        // }
-        // $this->result = mail($mailTo, $subject, $bodyMail, $headers); // отправка письма
-    // }
 
 }
--- views/thanks.php
+++ views/thanks.php
@@ -4,15 +4,28 @@ require_once 'views/templates/header.php';
 ?>
 
 <div class="wrapper">
-    <br>
-    <br>
-    <div>Спасибо, что пользуетесь нашим cервисом</div>
-    <div>Подарок направлен на ваш мэйл</div>
-    <br>
-    <br>
-    <a href="index.php" class="proect">Вернуться на главную страницу</a>
-    <br>
-    <br>
+    
+    <?php
+    if($mail == 1){
+        echo (
+        '<div class="thanks-image"></div>
+        <div class= "thanks-thanks"><div>Спасибо, что пользуетесь нашим cервисом!</div></div>
+        <div class= "thanks-fift"><div>Подарок направлен на ваш email.</div></div>
+        <div class="thanks-block">
+        <a href="index.php" class="thanks-button">НА ГЛАВНУЮ</a>
+        </div>'
+        );
+    } else {
+        echo (
+        '<div class="thanks-image-problem"></div>
+        <div class= "thanks-problem">При отправке подарка позникла ошибка. Просим вернуться на страницу подарка для уточнения email.</div>
+        <div class="thanks-block">
+        <a href="index.php?page=survey" class="thanks-button-problem">ВЕРНУТЬСЯ</a>
+        </div>'
+        );
+    }
+     
+    ?>
 </div>
 
 <?php
