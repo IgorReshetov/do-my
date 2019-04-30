3e286ce1d902add507af9d65c42aa2dcd3b8cdf2
Yurback <ryv1983@mail.ru>
2019-05-01 02:59:43 +0300

    Удалил лишний код по лису

--- views/js/fox.js
+++ views/js/fox.js
@@ -30,36 +30,20 @@ var fox = {
         var hello_old = this.words.hello_old[Math.floor(Math.random() * this.words.hello_old.length)];
         console.log(hello_old);
         
-        // switch (new_old.user) {
-        //     case "new":
-        // url("/do-my/views/images/icon/fox_wakeUp.svg") center center/cover 
-                setTimeout(function() {
-                    console.log(wakeUp);
-                    fox_img[0].style.background = "url(views/images/icon/fox_" + wakeUp + ".svg) center center/cover no-repeat";
-                    fox_img[0].classList.add('fox-js');
-                    fox_img[0].classList.remove('fox');
-                }, 2000, wakeUp);
-                setTimeout(function() {
-                    console.log(speak);
-                    fox_img_js[0].style.background = "url(views/images/icon/fox_" + speak + ".svg) center center/cover no-repeat";
-                    console.log((new_old[0].attributes.user.nodeValue == "old"));
-                    fox_speeak[0].innerHTML = (new_old[0].attributes.user.nodeValue == 'new') ? hello_new : hello_old;
-                    fox_speeak[0].style.display = 'block';
-                    setTimeout(function(){fox_speeak[0].innerHTML = "";},3000);
-                }, 3000, speak, new_old, hello_new, hello_old);
-        //     break;
-        //     case "old":
-        //         setTimeout(function() {
-        //             fox_img[0].style.background = "URL(do-my/views/images/icon/fox_" + this.status_IMG.wakeUp + ".svg";
-        //         }, 3000);
-        //         setTimeout(function() {
-        //             fox_img[0].style.background = "URL(do-my/views/images/icon/fox_" + this.status_IMG.speak + ".svg";
-        //             fox_speeak[0].innerHTML = this.hello_old;
-        //             fox_speeak[0].style.display = 'block';
-
-        //         }, 4000);
-        //     break;
-        // }
+        setTimeout(function() {
+            console.log(wakeUp);
+            fox_img[0].style.background = "url(views/images/icon/fox_" + wakeUp + ".svg) center center/cover no-repeat";
+            fox_img[0].classList.add('fox-js');
+            fox_img[0].classList.remove('fox');
+        }, 2000, wakeUp);
+        setTimeout(function() {
+            console.log(speak);
+            fox_img_js[0].style.background = "url(views/images/icon/fox_" + speak + ".svg) center center/cover no-repeat";
+            console.log((new_old[0].attributes.user.nodeValue == "old"));
+            fox_speeak[0].innerHTML = (new_old[0].attributes.user.nodeValue == 'new') ? hello_new : hello_old;
+            fox_speeak[0].style.display = 'block';
+            setTimeout(function(){fox_speeak[0].innerHTML = "";},3000);
+        }, 3000, speak, new_old, hello_new, hello_old);
     },
 
 }
