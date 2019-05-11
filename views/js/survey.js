0d8f076cdcd6f35b1274b230064f04b734e96887
Yurback <ryv1983@mail.ru>
2019-05-11 16:44:26 +0300

    поправил количество неправльных ответов

--- views/js/survey.js
+++ views/js/survey.js
@@ -810,13 +810,13 @@ function update_afterClientAnswer(otvet) {
 
 
     if ((check_arr[1] <= check_arr[0]) && (cookies.user_answer.length == (countQst_lev1 - 1))) {
-        if (otvet.answer_is_true == 1) ++levelQst_1.hit; 
+        if (otvet.answer_is_true == 1) {++levelQst_1.hit; --levelQst_1.miss;}
             else ++levelQst_1.miss;
     } else if ((check_arr[1] <= check_arr[0]) && (cookies.user_answer.length == (countQst_lev2 - 1))) {
-        if (otvet.answer_is_true == 1) levelQst_2.hit++; 
+        if (otvet.answer_is_true == 1) {levelQst_2.hit++; --levelQst_1.miss;}
             else levelQst_2.miss++;
     } else if ((check_arr[1] <= check_arr[0]) && (cookies.user_answer.length == (countQst_lev3 - 1))) {
-        if (otvet.answer_is_true == 1) levelQst_3.hit++; 
+        if (otvet.answer_is_true == 1) {levelQst_3.hit++; --levelQst_1.miss;}
             else levelQst_3.miss++;
     }
  
