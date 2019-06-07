4a9170560075f8dd46de7682d8fc990fdb6a6e65
Yurback <ryv1983@mail.ru>
2019-06-08 02:28:28 +0300

    настроил скрол до елемента

--- views/js/survey.js
+++ views/js/survey.js
@@ -196,13 +196,40 @@ function init() {
         perebor_Qst();
 
         setTimeout(function(){
-            var coord_target = offset(button1);
-            console.log (coord_target);
-            handl_scroll_el = setInterval(scroll_to_downPage,15,top_scroll_Y);
+            var el = O('land-block5-button');
+            console.log(el.getBoundingClientRect().top);
+            handl_scroll_el = setInterval(scroll_to_element,15,el);
             // $('html').animate({scrollTop: coord_target},2000);
 
             setTimeout(function(){C('land-block-text land-block5-button')[0].classList.add('land-block5-button-scroll')},2500);
+            setTimeout(function(){
+                var el = C("land-block-text land-block1-text")[0];
+                console.log(el.getBoundingClientRect().top);
+                handl_scroll_el = setInterval(scroll_to_element,15,el);
+            },3000);
+            setTimeout(function(){
+                var el = C("land-block-text land-block2-text")[0];
+                console.log(el.getBoundingClientRect().top);
+                handl_scroll_el = setInterval(scroll_to_element,15,el);
+            },6000);
+            setTimeout(function(){
+                var el = C("land-block-text land-block3-text")[0];
+                console.log(el.getBoundingClientRect().top);
+                handl_scroll_el = setInterval(scroll_to_element,15,el);
+            },8000);
+            setTimeout(function(){
+                var el = C("land-block-text land-block4-text")[0];
+                console.log(el.getBoundingClientRect().top);
+                handl_scroll_el = setInterval(scroll_to_element,15,el);
+            },10000);
+            setTimeout(function(){
+                var el = C("land-block-text land-block5-button land-block5-button-scroll")[0];
+                console.log(el.getBoundingClientRect().top);
+                handl_scroll_el = setInterval(scroll_to_element,15,el);
+            },12000);
+
         }, 5000);
+
     };
     
     var button2 = document.getElementById('button');
@@ -2059,25 +2086,76 @@ function perebor_Qst() {
 
 // _______________________________ S С R O L L (Плавная прокурутка страницы)______________________________________)
 
-var handl_scroll_el,
+var handl_scroll_down,
+    handl_scroll_el,                                                        // ссылка на сетинтервал для выключения скролла
     top_scroll_Y = 0,
-    body = document.body,
+    body = document.body,   
     html = document.documentElement,
     pageH_Y = Math.max( body.scrollHeight, body.offsetHeight, 
                 html.clientHeight, html.scrollHeight, html.offsetHeight ),
-    client_h_Y = document.documentElement.clientHeight;
+    client_h_Y = document.documentElement.clientHeight,
+    dist_scroll_check = 0,      // Растояние от верха окна до элемента
+    dist_scroll = 0,                                                      
+    scroll_top = 0,
+    scroll_begin = false,
+    step_scroll = 5;
 
 function scroll_to_downPage() {
     top_scroll_Y += 5;
     if (top_scroll_Y > (pageH_Y - client_h_Y)) {
-        clearInterval(handl_scroll_el);
+        clearInterval(handl_scroll_down);
     }
-    window.scrollTo(0,top_scroll_Y);     
+    scrollTo(0,top_scroll_Y);     
     // return false; 
 }
 
+function scroll_to_element(el) {
+    if (scroll_begin) {
+        if (dist_scroll_check>0) {
+            scroll_top = window.pageYOffset || document.documentElement.scrollTop;
+            if (dist_scroll <=0 || (scroll_top+step_scroll) > (pageH_Y - client_h_Y)) {
+                clearInterval(handl_scroll_el);
+                scroll_begin = false;
+                console.log(dist_scroll);
+                return false;
+            }
+            dist_scroll -= step_scroll;
+            top_scroll_Y = scroll_top + step_scroll;
+            scrollTo(0,top_scroll_Y);
+        }
+        else if (dist_scroll_check<0) {
+            scroll_top = window.pageYOffset || document.documentElement.scrollTop;
+            if (dist_scroll >= 0 || scroll_top==0) {
+                clearInterval(handl_scroll_el);
+                scroll_begin = false;
+                console.log(dist_scroll);
+                return false;
+            }
+            dist_scroll -= step_scroll;
+            top_scroll_Y = scroll_top + step_scroll;
+            scrollTo(0,top_scroll_Y);
+        } else {
+            clearInterval(handl_scroll_el);
+            scroll_begin = false;
+            return false;
+        }
+
+    } else {
+        dist_scroll = dist_scroll_check = el.getBoundingClientRect().top + el.clientHeight/2 - window.innerHeight/2;
+        scroll_top = window.pageYOffset || document.documentElement.scrollTop;
+            if (dist_scroll>0) step_scroll = 5;
+            else if (dist_scroll == 0) step_scroll = 0;
+            else step_scroll = -5;
+        pageH_Y = Math.max( body.scrollHeight, body.offsetHeight, 
+                        html.clientHeight, html.scrollHeight, html.offsetHeight ),
+        client_h_Y = document.documentElement.clientHeight;
+        scroll_begin = true;
+    }
+    
+}
+
 function offset(el) {
-    var rect = el.getBoundingClientRect().top + el.clientHeight/2 + document.documentElement.clientHeight/2;
+    var rect = el.getBoundingClientRect().top + el.clientHeight + document.documentElement.clientHeight/2;
     // console.log(rect);
     scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     var top = rect + scrollTop - document.documentElement.clientHeight;
