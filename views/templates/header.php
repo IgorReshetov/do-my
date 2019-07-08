c0a0e9c219caba7ccb42602240dcabf37d6f4341
IgorReshetov <ivreshetov@mail.ru>
2019-07-09 00:05:23 +0300

    Небольшие правки по условиям выбора и верстка

--- views/templates/header.php
+++ views/templates/header.php
@@ -138,23 +138,21 @@
     <script src="views/js/menu.js?ver=<?=VER?>"></script>
 
 
-<!-- Yandex.Metrika counter -->
-
+ <!-- Yandex.Metrika counter -->
 
 <!-- <script type="text/javascript" >
    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-   ym(53795302, "init", {
+   ym(54370369, "init", {
         clickmap:true,
         trackLinks:true,
         accurateTrackBounce:true,
         webvisor:true
    });
 </script>
-<noscript><div><img src="https://mc.yandex.ru/watch/53795302" style="position:absolute; left:-9999px;" alt="" /></div></noscript> -->
+<noscript><div><img src="https://mc.yandex.ru/watch/54370369" style="position:absolute; left:-9999px;" alt="" /></div></noscript> -->
 
-
-<!-- /Yandex.Metrika counter -->  
+<!-- /Yandex.Metrika counter -->
     
